523c6a8cce7fb82ccbec0010b8d2cc30044068cb
SATYAJIT-DAS <sjcdas@gmail.com>
2022-03-17 11:36:36 +0600

    twak pop up added

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -86,15 +86,15 @@
     </script>
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-        // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        // (function(){
-        //     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        //     s1.async=true;
-        //     s1.src='https://embed.tawk.to/620d4cb4a34c24564126a2d0/1fs1v78n5';
-        //     s1.charset='UTF-8';
-        //     s1.setAttribute('crossorigin','*');
-        //     s0.parentNode.insertBefore(s1,s0);
-        // })();
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/620d4cb4a34c24564126a2d0/1fs1v78n5';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
     </script>
     <!--End of Tawk.to Script-->
     <style>
